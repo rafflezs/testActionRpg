e7b35bb7e505a2d7bd047e51f4be60260194fab4
Rafflezs <thi-rafael@hotmail.com.br>
2021-05-26 13:41:49 -0300

    So close

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,10 +2,12 @@ extends KinematicBody2D
 
 const FRICTION = 500
 const ACCELERATION = 500
-const MAX_SPEED = 95
+const MAX_SPEED = 80
 var velocity = Vector2.ZERO
 
 onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	
@@ -15,17 +17,12 @@ func _physics_process(delta):
 	_input_vector = _input_vector.normalized()
 	
 	if _input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", _input_vector)
+		animation_tree.set("parameters/Walk/blend_position", _input_vector)
+		animation_state.travel("Walk")
 		velocity = velocity.move_toward(MAX_SPEED * _input_vector, delta * ACCELERATION)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
-
-	var mouse_position = get_viewport().get_mouse_position()
-	var direction = (mouse_position - self.position).normalized()
-	var new_angle = PI + atan2(direction.y, direction.x)
-	self.rotation = new_angle
-	
-	$Sprite.look_at(get_global_mouse_position())
-	$Sprite.rotation -= PI
-	$Sprite.rotation = new_angle

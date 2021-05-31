533b3b695bb90ff30efaa77ae749fd15de059d9e
Rafflezs <thi-rafael@hotmail.com.br>
2021-05-31 12:22:03 -0300

    Attack State Machine()

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,12 +5,28 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 var velocity = Vector2.ZERO
 
+enum {
+	MOVE,
+	ATACK
+}
+
+var state
+
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
-	
+	match state:
+		MOVE:
+			move_state(delta)
+		ATACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var _input_vector = Vector2.ZERO
 	_input_vector.x = (Input.get_action_strength("right") - Input.get_action_strength("left"))
 	_input_vector.y = (Input.get_action_strength("down") - Input.get_action_strength("up"))
@@ -26,3 +42,9 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+
+	if Input.is_action_just_pressed("ui_down"):
+		state = ATACK
+
+func attack_state(delta):
+	pass
